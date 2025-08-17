4279c23c95abb2023a4cb6f844ad87a02aeabf8d
Manjunatha Kinagi <kinagimanju@gmail.com>
2025-08-17 18:22:21 +0000

    you dont seem to follow industry best coding standards and you have intr

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,7 +4,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {"projectId":"naamkarann","appId":"1:359994149198:web:08708de51a6eedbbb8761f","storageBucket":"naamkarann.firebasestorage.app","apiKey":"AIzaSyA9Sj8gNVdgNhABaxGbmYFvmD2PWgnEp7E","authDomain":"naamkarann.firebaseapp.com","messagingSenderId":"359994149198"};
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
