a1aeb39afa1b4ea8656f8427d3b8e6da7074a8b3
Your Name <kinagimanju@gmail.com>
2025-08-17 03:16:24 +0000

    how can we avoid vertical scroll across the pages? i see there is enough

--- src/app/form/layout.tsx
+++ src/app/form/layout.tsx
@@ -10,7 +10,7 @@ export default function FormLayout({
     <div className="flex flex-col min-h-screen bg-background text-foreground">
       <div className="container mx-auto p-4 md:p-8 flex-grow">
         <FormHeader />
-        <main className="flex-grow flex items-start justify-center pt-8 pb-48">
+        <main className="flex-grow flex items-start justify-center pt-8">
           {children}
         </main>
       </div>
