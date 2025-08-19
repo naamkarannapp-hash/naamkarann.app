9d451da526a20a3ffbab7f89d8c0d4cb55fbbf98
Manjunatha Kinagi <kinagimanju@gmail.com>
2025-08-19 18:51:28 +0000

    while the build is happening ... will you be able to help me launch Naam

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import Providers from '@/components/providers';
+import Script from 'next/script';
 
 
 export const metadata: Metadata = {
@@ -21,6 +22,11 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <Script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
+          crossOrigin="anonymous"
+        />
       </head>
       <body className="font-body antialiased" suppressHydrationWarning>
         <Providers>
