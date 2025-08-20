80b411e969cb44e2d4a5f7b436cdc32f296221d9
Manjunatha Kinagi <kinagimanju@gmail.com>
2025-08-20 19:22:46 +0000

    add some 8 padding below logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,7 +69,7 @@ export default function Home() {
     <div className="flex flex-col min-h-screen bg-background text-foreground">
       <main className="flex-grow flex flex-col items-center text-center text-white indian-gradient">
           <div className="flex-grow flex flex-col items-center justify-center p-4 pt-8 md:pt-12">
-            <h2 className="text-4xl font-bold">Naamkarann<sup className="text-sm">&trade;</sup></h2>
+            <h2 className="text-4xl font-bold pb-8">Naamkarann<sup className="text-sm">&trade;</sup></h2>
             <div className="relative my-4 md:my-6">
                 <h1 className="font-headline text-5xl md:text-6xl font-bold leading-tight" style={{ textShadow: '0 2px 4px rgba(0,0,0,0.2)' }}>
                     The perfect baby name awaits.
