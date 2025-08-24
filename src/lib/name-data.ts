c119dcb6ce982da081b50be422f3f3f6043845ac
Manjunatha Kinagi <kinagimanju@gmail.com>
2025-08-24 14:28:36 +0000

    updated names

--- src/lib/name-data.ts
+++ src/lib/name-data.ts
@@ -1,12 +1,89 @@
 
 export const namesByTradition = {
-  "Hindu": [ "Aaranya", "Tejas", "Mandara", "Varnika", "Sarasangi", "Chandrakant", "Gitisha", "Nilay", "Dhruv", "Pushkar" ],
-  "Muslim": [ "Zahara", "Noor", "Rayhan", "Muneera", "Samaa", "Asrar", "Qamar", "Naqsh", "Aydin", "Naghma" ],
-  "Christian": [ "Eden", "Lucas", "Coralie", "Psalma", "Lucian", "Rosabel", "Verity", "Azure", "Gloria", "Clement" ],
-  "Sikh": [ "Amrit", "Gurpreet", "Surinder", "Harnoor", "Ravdeep", "Sukhman", "Basanti", "Kirtan", "Nirmaljit", "Baghpreet" ],
-  "Buddhist": [ "Lotusara", "Sujata", "Pema", "Dawa", "Sangita", "Bodhi", "Chandra", "Dhamma", "Saffron", "Anila" ],
-  "Jain": [ "Shrut", "Prakrit", "Harita", "Sumitra", "Nirjin", "Chaitra", "Dhavala", "Shanti", "Ratnesh", "Sargam" ],
-  "Jewish": [ "Talia", "Shira", "Orli", "Lev", "Keshet", "Ziv", "Dorit", "Erez", "Lior", "Yarden" ]
+  "Hindu": [
+    "Aranyani",   // forest goddess
+    "Viresha",    // strength, bravery
+    "Mandarani",  // celestial tree
+    "Taarunika",  // starlight
+    "Satyavani",  // truth-voice
+    "Chaitrali",  // blooming in spring
+    "Girikanta",  // beloved of the mountains
+    "Nilaya",     // ultimate abode
+    "Dharaani",   // earth-bearer
+    "Pushan",     // nourisher, Vedic deity
+  ],
+  "Muslim": [
+    "Zehraaneh",  // radiant blossom
+    "Nouriya",    // luminous, divine light
+    "Rayhanaar",  // heavenly fragrant herb
+    "Munirah",    // one who illumines
+    "Samawi",     // of the heavens
+    "Asraarun",   // keeper of secrets
+    "Qamariah",   // moonlit essence
+    "Naqshira",   // divine imprint
+    "Aydinah",    // little moon
+    "Naghmira"    // melodious soul
+  ],
+  "Christian": [
+    "Evangelia",  // bearer of good news
+    "Lucasta",    // pure light
+    "Coralith",   // jewel from the sea
+    "Psalmira",   // lyrical hymn
+    "Lumiel",     // divine radiance
+    "Rosavere",   // rose of truth
+    "Veritash",   // embodiment of truth
+    "Azureen",    // boundless sky
+    "Gloriana",   // immortal glory
+    "Clementis"   // gentle, merciful
+  ],
+  "Sikh": [
+    "Amritveer",   // warrior of the eternal nectar
+    "Gurshaan",    // pride of the Guru
+    "Surjodh",     // courageous in battle
+    "Harnooriah",  // illuminating God’s light
+    "Ravleen",     // absorbed in the sun-divine
+    "Sukhmira",    // ocean of peace
+    "Basandra",    // fragrance of devotion
+    "Kirtidar",    // bearer of divine songs
+    "Nirmata",     // pure creator
+    "Baghindra"    // lion of the gardens
+  ],
+  "Buddhist": [
+    "Lotushin",    // emerging lotus
+    "Sujatara",    // noble-born
+    "Pemavara",    // exalted lotus
+    "Dawajin",     // moon of wisdom
+    "Sangchen",    // great community
+    "Bodhira",     // seeker of awakening
+    "Chandnira",   // moonlit clarity
+    "Dhammapura",  // abode of dharma
+    "Kesariya",    // sacred saffron
+    "Anilaya"      // boundless freedom
+  ],
+  "Jain": [
+    "Shrutika",    // voice of scriptures
+    "Pravarit",    // excellence in purity
+    "Haridra",     // golden-yellow (turmeric, auspicious)
+    "Sumitara",    // friend of all beings
+    "Nirjaani",    // free from bondage
+    "Chaitratva",  // spirit of spring clarity
+    "Dhavalika",   // resplendent white
+    "Shantivar",   // bringer of peace
+    "Ratnidhi",    // treasure of jewels
+    "Sarganik",    // cosmic melody
+  ],
+  "Jewish": [
+    "Taliora",     // dew of divine light
+    "Shiryah",     // song of God
+    "Orliven",     // light of the heart
+    "Leviyah",     // lion-like heart
+    "Keshetiel",   // rainbow of God
+    "Zivora",      // radiant brilliance
+    "Dorital",     // generation of dew
+    "Erezian",     // cedar-born
+    "Lioren",      // my light guides
+    "Yardanael"    // flowing like the Jordan
+  ]
 };
 
 export const allNames = Object.values(namesByTradition).flat();
