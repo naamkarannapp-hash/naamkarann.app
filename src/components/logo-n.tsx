7da7031863640f60d11596ac207d61ff3b6c3396
Manjunatha Kinagi <kinagimanju@gmail.com>
2025-09-06 04:07:25 +0000

    See the logo on the top right corner.

--- src/components/logo-n.tsx
+++ src/components/logo-n.tsx
@@ -10,13 +10,14 @@ export const NaamkarannLogoN = React.forwardRef<SVGSVGElement, React.SVGProps<SV
                 className={cn(className)}
                 viewBox="0 0 24 24"
                 fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 xmlns="http://www.w3.org/2000/svg"
                 {...props}
             >
-                <path
-                    d="M16.8521 21V6.25L9.61208 17.65V2.89999H7.15208V21H9.61208L16.8521 9.55V21H16.8521Z"
-                    fill="currentColor"
-                />
+                <path d="M7 13l5 5 5-5" />
             </svg>
         );
     }
