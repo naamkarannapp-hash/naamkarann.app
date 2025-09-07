05d69b4771c4aae3c4c80d2c1d5ff01f428e70ee
Manjunatha Kinagi <kinagimanju@gmail.com>
2025-09-07 06:30:58 +0000

    This webpage is not using the Strict-Transport-Security header! This is

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -17,6 +18,20 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+        ],
+      },
+    ]
   },
 };
 
